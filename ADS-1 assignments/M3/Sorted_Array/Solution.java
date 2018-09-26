20038847851302b48556648593de5e4065ee3e37
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-09-26 21:35:47 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- ADS-1 assignments/M3/Sorted_Array/Solution.java
+++ ADS-1 assignments/M3/Sorted_Array/Solution.java
@@ -25,6 +25,12 @@ class SortedArray {
      * newarray.
      */
     private int[] newarray;
+    /**
+     * Constructs the object.
+     *
+     * @param      arr1   The archive 1
+     * @param      arr2   The archive 2
+     */
     SortedArray(final int[] arr1, final int[] arr2) {
         this.array1 = arr1;
         this.size1 = array1.length;
@@ -32,6 +38,14 @@ class SortedArray {
         this.size2 = array2.length;
         this.newarray = new int[size1 + size2];
     }
+    /**
+     * {Method to sortArray the array}.
+     *
+     * @param      a1    Array 1
+     * @param      a2    Array 2
+     *
+     * @return     {Array}
+     */
     public int[] sortArray(final int[] a1, final int[] a2) {
         int i = 0, j = 0, k = 0;
         while (i < size1 && j < size2) {
@@ -50,7 +64,9 @@ class SortedArray {
         return this.newarray;
     }
 }
-
+/**
+ * Class for solution.
+ */
 public final class Solution {
     /**
      * Constructs the object.
@@ -58,6 +74,11 @@ public final class Solution {
     private Solution() {
         //Empty.
     }
+    /**
+     * {Main method}.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int first = Integer.parseInt(scan.nextLine());
