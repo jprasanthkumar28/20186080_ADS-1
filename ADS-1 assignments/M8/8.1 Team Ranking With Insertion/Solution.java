31e08fd481d8a75fad3799dc15b8795ae2e5389a
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-10-02 17:37:56 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-1 assignments/M8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1 assignments/M8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,46 +1,142 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for cricket.
+ */
 class Cricket implements Comparable<Cricket> {
+	/**
+	 * teamName varaible.
+	 */
     private String teamName;
+    /**
+     * wins varaible.
+     */
     private int wins;
+    /**
+     * losses varaible.
+     */
     private int losses;
+    /**
+     * draws varaible.
+     */
     private int draws;
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     */
     Cricket() {
+    	/**
+    	 * teamName varaible..
+    	 */
         this.teamName = null;
+        /**
+         * for wins varaible.
+         */
         this.wins = 0;
+        /**
+         * losses variable.
+         */
         this.losses = 0;
+        /**
+         * draws varaible.
+         */
         this.draws = 0;
     }
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     *
+     * @param      name  The name
+     * @param      win   The window
+     * @param      loss  The loss
+     * @param      draw  The draw
+     */
     Cricket(final String name, final int win, final int loss, final int draw) {
         this.teamName = name;
         this.wins = win;
         this.losses = loss;
         this.draws = draw;
     }
+    /**
+     * Gets the team name.
+     * Time complexity is 1.
+     *
+     * @return     The team name.
+     */
     public String getTeamName() {
         return this.teamName;
     }
+    /**
+     * Gets the wins.
+     * Time complexity is 1.
+     *
+     * @return     The wins.
+     */
     public int getWins() {
         return this.wins;
     }
+    /**
+     * Gets the losses.
+     * Time complexity is 1.
+     *
+     * @return     The losses.
+     */
     public int getLosses() {
         return this.losses;
     }
+    /**
+     * Gets the draws.
+     * Time complexity is 1.
+     *
+     * @return     The draws.
+     */
     public int getDraws() {
         return this.draws;
     }
+    /**
+     * Sets the name.
+     * Time complexity is 1.
+     *
+     * @param      s     { parameter_description }
+     */
     public void setName(final String s) {
         this.teamName = s;
     }
+    /**
+     * Sets the wins.
+     * Time complexity is 1.
+     *
+     * @param      w     { parameter_description }
+     */
     public void setWins(final int w) {
         this.wins = w;
     }
+    /**
+     * Sets the losses.
+     * Time complexity is 1.
+     *
+     * @param      l     { parameter_description }
+     */
     public void setLosses(final int l) {
         this.losses = l;
     }
+    /**
+     * Sets the draws.
+     * Time complexity is 1.
+     *
+     * @param      d     { parameter_description }
+     */
     public void setDraws(final int d) {
         this.draws = d;
     }
+    /**
+     * to compare the details based on wins,losses and draws.
+     * Time complexity is 1.
+     *
+     * @param      that  The that
+     *
+     * @return     1 if it is true or else -1.
+     */
     public int compareTo(final Cricket that) {
         if (this.getWins() < that.getWins()) {
             return 1;
@@ -71,26 +167,59 @@ class Cricket implements Comparable<Cricket> {
     }
 
 }
+/**
+ * Class for selection sort.
+ */
 class SelectionSort {
+	/**
+	 * to store the champions.
+	 */
     private Cricket[] champions;
+    /**
+     * to get the size.
+     */
     private int size;
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     */
     SelectionSort() {
         final int x = 10;
         this.champions = new Cricket[10];
         this.size = 0;
     }
+    /**
+     * to resize the array.
+     * Time complexity is 1.
+     */
     public void resize() {
         champions = Arrays.copyOf(champions, 2 * champions.length);
     }
+    /**
+     * Gets the size.
+     * Time complexity is 1.
+     *
+     * @return     The size.
+     */
     public int getSize() {
         return this.size;
     }
+    /**
+     * to add the data of cricket class.
+     * Time complexity is 1.
+     *
+     * @param      t     { parameter_description }
+     */
     public void add(final Cricket t) {
         if (size == champions.length) {
             resize();
         }
         champions[size++] = t;
     }
+    /**
+     * to sort the elements based on wins, losses, draws.
+     * Time complexity is N * N.
+     */
     public void sort() {
         for (int i = 1; i < size; i++) {
             Cricket tour = champions[i];
@@ -123,6 +252,10 @@ class SelectionSort {
 	// 	else break;
 	// }
 
+    /**
+     * to display the data of class.
+     * Time complexity is N.
+     */
     public void display() {
         int i = 0;
         for (i = 0; i < size - 1; i++) {
@@ -131,7 +264,13 @@ class SelectionSort {
         System.out.println(champions[i].getTeamName());
     }
 }
+/**
+ * Class for solution.
+ */
 public class Solution {
+	/**
+	 * Constructs the object.
+	 */
     private Solution() {
         //Empty Contructor.
     }
