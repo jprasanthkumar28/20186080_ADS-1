b27444c2020114aa8a3aeffa15aee343511ca8f4
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-10-03 22:59:58 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1 assignments/M9/9.1 Merge Sort Improvements/Solution.java
+++ ADS-1 assignments/M9/9.1 Merge Sort Improvements/Solution.java
@@ -1,20 +1,51 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for merge sort.
+ */
 class MergeSort {
-    // cutoff to insertion sort
+    /**
+     * final variable.
+     */
     private final int Seven = 7;
+    /**
+     * int array size variable.;;
+     */
     private int[] array;
+    /**
+     * size variable.
+     */
     private int size;
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     */
     MergeSort() {
         this.array = new int[1];
         this.size = 0;
     }
+    /**
+     * Gets the size.
+     * Time complexity is 1.
+     *
+     * @return     The size.
+     */
     public int getSize() {
         return this.size;
     }
+    /**
+     * to resize the gievn array.
+     * Time complexity is N.
+     */
     private void resize() {
         array = Arrays.copyOf(array, 2 * array.length);
     }
+    /**
+     * to add the elements into the array.
+     * Time complexity is 1.
+     *
+     * @param      item  The item
+     */
     public void add(final int item) {
         if (size == array.length) {
             resize();
@@ -23,13 +54,19 @@ class MergeSort {
     }
     /**
      * Rearranges the array in ascending order, using the natural order.
+     *
+     * @param      a     Comparable object.
+     * Time complexity is O(N).
      */
     public void sort(Comparable[] a) {
         Comparable[] aux = a.clone();
         sort(aux, a, 0, a.length - 1);
         assert isSorted(a);
     }
-    // sort from a[lo] to a[hi] using insertion sort
+    /**
+     * insertion sort.
+     * Time complexity is O(N).
+     */
     public void insertionSort(Comparable[] a, int lo, int hi) {
         for (int i = lo; i <= hi; i++) {
             for (int j = i; j > lo && less(a[j], a[j - 1]); j--) {
@@ -37,17 +74,52 @@ class MergeSort {
             }
         }
     }
+    /**
+     * to exchage or to swao to swap.
+     * Time complexity is O(N).
+     *
+     * @param      a     { parameter_description }
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
     public void exch(Object[] a, int i, int j) {
         Object swap = a[i];
         a[i] = a[j];
         a[j] = swap;
     }
+    /**
+     * to check two comparable objects.
+     * Time complexity is O(1).
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     *
+     * @return     true or false based on the condition.
+     */
     public boolean less(Comparable a, Comparable b) {
         return a.compareTo(b) < 0;
     }
+    /**
+     * Determines if sorted.
+     * Time complexity is O(1).
+     *
+     * @param      a     { parameter_description }
+     *
+     * @return     True if sorted, False otherwise.
+     */
     public boolean isSorted(Comparable[] a) {
         return isSorted(a, 0, a.length - 1);
     }
+    /**
+     * Determines if sorted.
+     * Time complexity is O(log(N)).
+     *
+     * @param      a     { parameter_description }
+     * @param      lo    The lower
+     * @param      hi    The higher
+     *
+     * @return     True if sorted, False otherwise.
+     */
     public boolean isSorted(Comparable[] a, int lo, int hi) {
         for (int i = lo + 1; i <= hi; i++) {
             if (less(a[i], a[i - 1])) {
@@ -56,6 +128,16 @@ class MergeSort {
         }
         return true;
     }
+    /**
+     * to merge the data.
+     * Time complexity is O(N).
+     *
+     * @param      array  The array
+     * @param      aux    The auxiliary
+     * @param      lo     The lower
+     * @param      mid    The middle
+     * @param      hi     The higher
+     */
     public void merging(Comparable[] array, Comparable[] aux, int lo, int mid, int hi) {
         assert isSorted(array, lo, mid);
         assert isSorted(array, mid + 1, hi);
@@ -74,6 +156,15 @@ class MergeSort {
         }
         assert isSorted(aux, lo, hi);
     }
+    /**
+     * To sort.
+     * Time complexity is O(Log(N)).
+     *
+     * @param      array  The array
+     * @param      aux    The auxiliary
+     * @param      lo     The lower
+     * @param      hi     The higher
+     */
     public void sort(Comparable[] array, Comparable[] aux, int lo, int hi) {
         if (hi <= lo + Seven) {
             insertionSort(aux, lo, hi);
@@ -97,7 +188,7 @@ class MergeSort {
         String str = "[";
         int i;
         for (i = 0; i < a.length - 1; i++) {
-            str += a[i] + ",";
+            str += a[i] + ", ";
         }
         str += a[i] + "]";
         return str;
