935ae16c3a33621e4e61492e89d551e20042a125
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-10-10 17:46:50 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/M15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-1 assignments/M15/15.1 BinarySearchSymbolTable/Solution.java
@@ -348,15 +348,17 @@ class BinarySearchST<Key extends Comparable<Key>, Value> {
         if (lo == null) {
             throw new 
             IllegalArgumentException("first argument to size() is null");
-        } if (hi == null) {
+        } 
+        if (hi == null) {
             throw new 
-            IllegalArgumentException("second argument to size() is null");
-        } if (lo.compareTo(hi) > 0) {
+            IllegalArgumentException("second argument to size() is null");  
+        }  
+        if (lo.compareTo(hi) > 0) {
             return 0;
-        } if (contains(hi)) {
-            return rank(hi) - rank(lo) + 1;
         }
-        else {
+        if (contains(hi)) {
+            return rank(hi) - rank(lo) + 1;
+        } else {
             return rank(hi) - rank(lo);
         }
     }
